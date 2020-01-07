f48de60de396c800b3d42e2a5b3b1d2f701b0582
Recruit <you@example.com>
2020-01-07 14:53:25 +0200

    Logging added to the visitor class

--- src/main/java/Visitor.java
+++ src/main/java/Visitor.java
@@ -1,21 +1,27 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.io.File;
 import java.io.IOException;
 
 public class Visitor {
-
+private static final Logger logger = LogManager.getLogger(Main.class.getName());
     public void save() {
         try {
             File visitor = new File("visitor_alice.txt");
 
             if(visitor.createNewFile())
             {
-                System.out.println("File successfully created: " + visitor.getName()); //logger.info
+                logger.info("File successfully created: " + visitor.getName());
+                //System.out.println("File successfully created: " + visitor.getName()); //logger.info
             }
             else{
-                System.out.println(visitor.getName() + " file already exists or was not successfully created "); //logger.error
+                logger.error(visitor.getName() + " file already exists");
+                //System.out.println(visitor.getName() + " file already exists or was not successfully created "); //logger.error
             }
         } catch (IOException e) {
-            System.out.println("An error occurred"); //logger.error
+            logger.error("An error occurred");
+            //System.out.println("An error occurred"); //logger.error
             e.printStackTrace();
         }
     }
