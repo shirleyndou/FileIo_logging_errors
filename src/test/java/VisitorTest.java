e3bef877e674e2f7f21449f8f666ebdab36657da
Recruit <you@example.com>
2020-01-27 16:19:21 +0200

    made changes to the log file

---
+++ src/test/java/VisitorTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+class VisitorTest {
+
+    @Test
+    public void save(){
+        Visitor alice = new Visitor("Alice Cooper", 54, "20/03/2020", "15h00", "No comments", "Victoria Secrets");
+
+
+    }
+
+
+
+}
