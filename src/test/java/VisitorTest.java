305e75cdb89168b9e5419e66ac7ada7d8070ffc6
Recruit <you@example.com>
2020-01-28 09:46:49 +0200

    an attempt to do tests

--- src/test/java/VisitorTest.java
+++ src/test/java/VisitorTest.java
@@ -1,12 +1,17 @@
 import org.junit.jupiter.api.Test;
+
+import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 
 class VisitorTest {
 
-    @Test
-    public void save(){
-        Visitor alice = new Visitor("Alice Cooper", 54, "20/03/2020", "15h00", "No comments", "Victoria Secrets");
+    Visitor alice = new Visitor("Alice Cooper", 54, "20/03/2020", "15h00", "No comments", "Victoria Secrets");
 
+    @Test
+    public void save() throws Exception{
+       alice.save();
 
     }
 
