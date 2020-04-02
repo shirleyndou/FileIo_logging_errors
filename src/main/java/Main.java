6575ce6fdb674dea8ef0b1a6d2666921a4b8f8a9
shirleyndou <shirley.ndou@umuzi.org>
2020-04-02 16:48:41 +0200

    FileIO update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,11 +3,20 @@ public class Main {
     public static void main(String[] args) {
 
         //class object
-        Visitor alice = new Visitor("Shirley Ndou", 54, "20/03/2020", "15h00", "No comments", "Victoria Secrets");
+        Visitor alice = new Visitor("Alice Copper", 54, "20/03/2020", "15h00", "No comments", "Victoria Secrets");
        //invoking the save method
         alice.save();
         //invoking the load method
         alice.load("Alice Cooper");
+
+        System.out.println("=======================================================================================================================================================");
+
+        //class object
+        Visitor Bob = new Visitor("Bob", 44, "30/11/2020", "13h00", "No comments", "Miss Mops");
+        //invoking the save method
+        Bob.save();
+        //invoking the load method
+        Bob.load("Alice Cooper");
 
     }
 }
